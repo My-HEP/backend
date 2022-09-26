ad0e9ca51bed0bc85645d8e69dbfc24f3ab924b2
Kristen Coy <kristenwcoy@gmail.com>
2022-09-26 15:55:25 -0400

    Continue routing efforts

--- src/routes/sharedRoutes.js
+++ src/routes/sharedRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/sharedController');
+const { PrismaClient } = require('@prisma/client');
+const database = new PrismaClient();
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/', (req, res) => {
-  res.send('user get request');
+
+router.post('/user', async (req, res) => {
+  const uid = req.body;
+  console.log(uid);
+  // const userData = await database.user.findUnique({ where: { uid: uid } });
+  // console.log(userData);
+
+  res.send(uid);
 });
 
 module.exports = router;
