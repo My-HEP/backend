2d3418a0c682bd0217d4fe8248df27c878e3d56c
Kayla Alewine <kayla.alewine@bah.com>
2022-10-19 16:27:45 -0400

    Remove loginUser and add prisma client to shared routes

--- src/routes/sharedRoutes.js
+++ src/routes/sharedRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
+const { PrismaClient } = require('@prisma/client');
+const database = new PrismaClient();
+
 const {
   createUserAccount,
-  loginUser,
   returnUserData,
 } = require('../controllers/sharedController');
 
 router.post('/createUserAccount', createUserAccount);
 router.post('/user', returnUserData);
+
 
 module.exports = router;
