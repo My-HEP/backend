e746101f880ed0aedad83d0e8b19d108c1a3d43d
Kayla Alewine <kayla.alewine@bah.com>
2022-10-25 15:59:42 -0400

    Create addHEPExercise route and controller

--- src/controllers/therapistController.js
+++ src/controllers/therapistController.js
@@ -131,6 +131,33 @@ const getPatient = async (req, res) => {
  res.json(patient);
 }
 
+// Add HEP exercise 
+const addHEPExercise = async (req, res) => {
+  const { exerciseId, frequency, duration, notes, patientId, assignedById } = req.body;
+  console.log(req.body)
+  // try {
+  //   const response = await database.HEPExercise.create({
+  //     data: {
+  //       // id: id,
+  //       // this was failing before I changed
+  //       // the default on the  exercise schema to
+  //       // uuid() instead of autoincrement()
+  //       // was requiring an id to be sent in the
+  //       // req.body
+  //       url: url,
+  //       title: title,
+  //       tags: {
+  //         connect: tags,
+  //       },
+  //     },
+  //   });
+  //   console.log(response);
+  //   res.status(200).json(response);
+  // } catch (error) {
+  //   res.status(500).json({ error: 'Something went wrong. Try again later.' });
+  // }
+}
+
 module.exports = {
   homeStats,
   addExercise,
@@ -138,5 +165,6 @@ module.exports = {
   getExercises,
   addPatient,
   getPatient,
+  addHEPExercise,
   deletePatient,
 };
